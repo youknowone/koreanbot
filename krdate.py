2aed4126c62a2d0c6d0c384808165e63a6ebfdfb
Jeong YunWon <jeong@youknowone.org>
2013-07-16 03:09:02 +0900

    fix bugs and clean up

--- krdate.py
+++ krdate.py
@@ -9,16 +9,13 @@ from bot import client
 
 import settings
 
-@client.msgevents.hookback('date', u'일자')
+@client.msgevents.hookback('date', u'날짜')
 def on_date(context, message=None):
     u"""Prints the date in KR time."""
-    if message is None:
-        return u'Please suggest keyword'
 
     now = datetime.now(timezone('Asia/Seoul'))
     if now.hour < 12:
         fmt = u'%a %Y년 %m월 %d일 오전 %I:%M:%S %z %Z'
-        return now.strftime(fmt.encode('utf-8')).decode('utf-8')
-    else
+    else:
         fmt = u'%a %Y년 %m월 %d일 오후 %I:%M:%S %z %Z'
-        return now.strftime(fmt.encode('utf-8')).decode('utf-8')
+    return now.strftime(fmt.encode('utf-8')).decode('utf-8')
