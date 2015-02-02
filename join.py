e65129dd3dddd0d24dd67d3293f555af254f9634
Henry Colomb <henry@henrycolomb.com>
2015-02-02 05:14:20 +0000

    Fix spacing issue on line 23

--- join.py
+++ join.py
@@ -20,7 +20,7 @@ def on_join(connection, sender, chan):
     if not name in users:
         if name[:8] == 'ChangeMe':
             connection.privmsg(chan, u'안녕하세요 {nick}님! Welcome to {chan}! You can change your name with "/nick new_name".'.format(chan=chan, nick=identity.nick))
-    	else:
+        else:
 	    connection.privmsg(chan, u'안녕하세요 {nick}님! Welcome to {chan}!'.format(chan=chan, nick=identity.nick))
             users.append(name)
             visitors._set(chan, users)
